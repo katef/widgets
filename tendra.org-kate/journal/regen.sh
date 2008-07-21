b7b26b6e7980e73248ff8417cc97c81b7ed2f6f3
Kate F <kate@elide.org>
2008-07-21 13:23:44 +0000

    Added regeneration.

--- tendra.org-kate/journal/regen.sh
+++ tendra.org-kate/journal/regen.sh
@@ -3,9 +3,11 @@
 
 # Regnerate journal entries
 
+# $1 - dest
+# $2 - src
 contents() {
-	src="$1"
-	file="$2"
+	file="$1"
+	src="$2"
 
 	y="`echo $src | cut -d/ -f 2`"
 	m="`echo $src | cut -d/ -f 3`"
@@ -37,39 +39,87 @@ entry() {
 	echo '<!--#include virtual="../../../../entry.inc" -->' > "$file"
 	echo '<!--#include virtual="../../../../cal.inc" -->' >> "$file"
 
-	contents "$1" "$file"
+	contents "$file" "$1"
 
 	echo '<!--#include virtual="../../../../footer.inc" -->' >> "$file"
 }
 
-recent() {
-	file="$1"
+year() {
+	dir=` echo $1 | cut -d/ -f 2`
+	file="$dir/index.sxhtml"
 
-	contents "$2" "$file"
+	mkdir -p "$dir"
+
+	echo '<!--#include virtual="../year.inc" -->' > "$file"
+
+	iterate 20 "src/$dir/??/??/*.html" "$file"
+
+	echo '<!--#include virtual="../footer.inc" -->' >> "$file"
 }
 
-for i in src/*/*/*/*.html; do
+# $1 - Number of items
+# $2 - glob, e.g. src/????/??/??/*.html
+gettoplist() {
+	ls -1d $2 \
+		| sed -E 's,^src/([0-9]{4})/([0-9]{2})/([0-9]{2})/(.+)\.html$,\1\2\3/\4,' \
+		| sort -rn \
+		| head -n $1 \
+		| sed -E 's,^([0-9]{4})([0-9]{2})([0-9]{2})/(.+)$,src/\1/\2/\3/\4.html,'
+}
+
+# Iterate through a list, calling a callback for each
+# $1 - number of items
+# $2 - glob
+# $3 - file
+iterate() {
+	IFS='|'
+	for i in `gettoplist $1 "$2" \
+		| tr '\n' '|'`; do
+		contents "$3" "$i"
+	done
+}
+
+date_year=`date +%Y`
+date_mon=`date +%B`
+date_day="`date +%e`"
+
+# Calendar
+echo '<div class="cal">' > cal.inc
+echo '<table>' >> cal.inc
+echo "<tr><th>&lt;</th><th colspan='5'>$date_mon</th><th>&gt;</th></tr>" >> cal.inc
+cal \
+	| grep -v $date_year \
+	| sed -E 's,(..)( |$),<td>\1</td>,g; s,$,</td></tr>,; s,^,<tr>,' \
+	| sed -E "s,$date_day,<em>&</em>," \
+	>> cal.inc
+echo '</table>' >> cal.inc
+echo '<hr/>' >> cal.inc
+
+for i in `ls -1d src/???? | cut -f 2 -d / | sort -n`; do
+	yy=`echo $i | cut -c 3-`
+	echo "<a href=\"$i\">&rsquo;$yy</a> " >> cal.inc
+done
+
+echo '</div>' >> cal.inc
+
+# Single entries
+for i in src/????/??/??/*.html; do
 	entry "$i"
 done
 
+# Years
+for i in src/????/??/??/*.html; do
+	year "$i"
+done
 
 
-
-# TODO: most recent:
+# Most recent
 file=index.sxhtml
 
 echo '<!--#include virtual="recent.inc" -->' > "$file"
 echo '<!--#include virtual="cal.inc" -->' >> "$file"
 
-IFS='|'
-for i in `ls -1d src/*/*/*/*.html \
-	| sed -E 's,^src/([0-9]{4})/([0-9]{2})/([0-9]{2})/(.+)\.html$,\1\2\3/\4,' \
-	| sort -rn \
-	| head -n 20 \
-	| sed -E 's,^([0-9]{4})([0-9]{2})([0-9]{2})/(.+)$,src/\1/\2/\3/\4.html,' \
-	| tr '\n' '|'`; do
-	recent "$file" "$i"
-done
+iterate 20 'src/????/??/??/*.html' "$file"
 
 echo '<!--#include virtual="footer.inc" -->' >> "$file"
 
