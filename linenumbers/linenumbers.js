04928820f4d9e45d1c39143344d8874221ef99b8
Kate F <kate@elide.org>
2010-01-04 12:10:22 +0000

    Moved Linenumbers into its own namespace.

--- linenumbers/linenumbers.js
+++ linenumbers/linenumbers.js
@@ -1,38 +1,43 @@
 /* $Id$ */
 
-function init_linenumbers() {
-	var pre = document.getElementsByTagName('pre'); 
-	if (pre == null) {
-		return;
-	}
+var Linenuumbers = new (function () {
 
-	for (var i = 0; i < pre.length; i++) {
-		var count;
-		var content;
-
-		content = pre[i].innerHTML;
-		content = content.replace(/\n+$/g, '');
-		content = content.replace(/\t/g, '    ');
-		pre[i].innerHTML = content;
-
-		/* IE replaces '\n' by a space; this degrades to match nothing. */
-		var matches = pre[i].innerHTML.match(/\n/mg);
-		if (matches == null) {
-			count = 1;
-		} else {
-			count = matches.length + 1;
+	this.init = function(root) {
+		var pre = root.getElementsByTagName('pre'); 
+		if (pre == null) {
+			return;
 		}
 
-		var numbers = [];
-		for (var n = 1; n <= count; n++) {
-			numbers.push(n);
-		}
-		numbers = numbers.join("\n");
+		for (var i = 0; i < pre.length; i++) {
+			var count;
+			var content;
 
-		var div = document.createElementNS('http://www.w3.org/1999/xhtml', 'div');
-		div.appendChild(document.createTextNode(numbers));
-		div.setAttribute('class', 'lines');
-		pre[i].appendChild(div);
-		pre[i].insertBefore(div, pre[i].firstChild);
+			content = pre[i].innerHTML;
+			content = content.replace(/\n+$/g, '');
+			content = content.replace(/\t/g, '    ');
+			pre[i].innerHTML = content;
+
+			/* IE replaces '\n' by a space; this degrades to match nothing. */
+			var matches = pre[i].innerHTML.match(/\n/mg);
+			if (matches == null) {
+				count = 1;
+			} else {
+				count = matches.length + 1;
+			}
+
+			var numbers = [];
+			for (var n = 1; n <= count; n++) {
+				numbers.push(n);
+			}
+			numbers = numbers.join("\n");
+
+			var div = root.ownerDocument.createElementNS('http://www.w3.org/1999/xhtml', 'div');
+			div.appendChild(root.ownerDocument.createTextNode(numbers));
+			div.setAttribute('class', 'lines');
+			pre[i].appendChild(div);
+			pre[i].insertBefore(div, pre[i].firstChild);
+		}
 	}
-}
+
+});
+
