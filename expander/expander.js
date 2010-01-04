fd9c4f55e8a9d99c0d7f1805d7fda31b971d2141
Kate F <kate@elide.org>
2010-01-04 10:08:31 +0000

    Moved into its own namespace.

--- expander/expander.js
+++ expander/expander.js
@@ -1,83 +1,87 @@
 /* $Id$ */
 
-function expand_classcontains(e, needle) {
-	var a = e.getAttribute("class").split(" ");
+var Expander = new (function () {
 
-	for (var i = 0; i < a.length; i++) {
-		if (a[i] == needle) {
-			return true;
+	function classcontains(e, needle) {
+		var a = e.getAttribute("class").split(" ");
+
+		for (var i = 0; i < a.length; i++) {
+			if (a[i] == needle) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	function classreplace(e, replacement) {
+		var i;
+
+		a = e.getAttribute("class").split(" ");
+
+		for (i = 0; i < a.length; i++) {
+			if (a[i] == "collapsed" || a[i] == "expanding" || a[i] == "expanded") {
+				a[i] = replacement;
+				break;
+			}
+		}
+
+		if (i == a.length) {
+			a.push(replacement);
+		}
+
+		e.setAttribute("class", a.join(" "));
+	}
+
+	this.down = function (a) {
+		/* TODO: hold diagonal */
+	}
+
+	this.expand = function (a) {
+		var dl = a.parentNode.parentNode;
+		var endclass;
+
+		if (classcontains(dl, "expanded")) {
+			endclass = "collapsed";
+		} else {
+			endclass = "expanded";
+		}
+
+		if (classcontains(dl, endclass)) {
+			return;
+		}
+
+		classreplace(dl, "expanding");
+
+		window.setTimeout(function() {
+				classreplace(dl, endclass);
+			}, 50);
+	}
+
+	/* TODO: can this be added to a stack of init functions? */
+	this.init = function (root) {
+		var dl = root.getElementsByTagName("dl");
+
+		/* Here I would use XPath, if it were supported... */
+		for (var i = 0; i < dl.length; i++) {
+			if (!classcontains(dl[i], "expandable")) {
+				continue;
+			}
+
+			var dt = dl[i].getElementsByTagName("dt")[0];
+
+			var a = document.createElementNS('http://www.w3.org/1999/xhtml', 'a');
+			a.setAttribute("onclick", "javascript:Expander.expand(this)");
+			a.setAttribute("onmousedown", "javascript:Expander.down(this)");
+			a.innerHTML  = dt.innerHTML;
+			dt.innerHTML = null;
+			dt.appendChild(a);
+
+			if (!classcontains(dl[i], "expanded")) {
+				classreplace(dl[i], "collapsed");
+			}
 		}
 	}
 
-	return false;
-}
-
-function expand_classreplace(e, replacement) {
-	var i;
-
-	a = e.getAttribute("class").split(" ");
-
-	for (i = 0; i < a.length; i++) {
-		if (a[i] == "collapsed" || a[i] == "expanding" || a[i] == "expanded") {
-			a[i] = replacement;
-			break;
-		}
-	}
-
-	if (i == a.length) {
-		a.push(replacement);
-	}
-
-	e.setAttribute("class", a.join(" "));
-}
-
-function expand_down(a) {
-	/* TODO: hold diagonal */
-}
-
-function expand(a) {
-	var dl = a.parentNode.parentNode;
-	var endclass;
-
-	if (expand_classcontains(dl, "expanded")) {
-		endclass = "collapsed";
-	} else {
-		endclass = "expanded";
-	}
-
-	if (expand_classcontains(dl, endclass)) {
-		return;
-	}
-
-	expand_classreplace(dl, "expanding");
-
-	window.setTimeout(function() {
-			expand_classreplace(dl, endclass);
-		}, 50);
-}
-
-/* TODO: can this be added to a stack of init functions? */
-function init_expander() {
-	var dl = document.getElementsByTagName("dl");
-
-	/* Here I would use XPath, if it were supported... */
-	for (var i = 0; i < dl.length; i++) {
-		if (!expand_classcontains(dl[i], "expandable")) {
-			continue;
-		}
-
-		var dt = dl[i].getElementsByTagName("dt")[0];
-
-		var a = document.createElementNS('http://www.w3.org/1999/xhtml', 'a');
-		a.setAttribute("onclick", "javascript:expand(this)");
-		a.setAttribute("onmousedown", "javascript:expand_down(this)");
-		a.innerHTML  = dt.innerHTML;
-		dt.innerHTML = null;
-		dt.appendChild(a);
-
-		if (!expand_classcontains(dl[i], "expanded")) {
-			expand_classreplace(dl[i], "collapsed");
-		}
-	}
-}
+});
 
