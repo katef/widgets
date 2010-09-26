94b7d2cfb84b722fec67bb770d0b0c20f43fc26a
Kate F <kate@elide.org>
2010-09-26 05:42:21 +0000

    A silly snprintf.

---
+++ snprintf/snprintf.js
@@ -0,0 +1,13 @@
+/* $Id$ */
+
+/*
+ * A javascript implementation of snprintf.
+ * TODO: explain dialect. based on C99
+ * TODO: explain use: add to string object?
+ */
+
+function snprintf(
+
+you know what? i can't be bothered to write this
+
+
