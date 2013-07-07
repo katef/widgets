11ce3a58598bc009e61f92d9a64644a52645d878
Kate F <kate@elide.org>
2013-07-07 18:36:08 +0000

    More generic javascript stuff.

---
+++ js/linenumbers.js
@@ -0,0 +1,43 @@
+/* $Id: linenumbers.js 2444 2010-11-06 22:30:28Z kate $ */
+
+var Linenumbers = new (function () {
+
+	this.init = function(root) {
+		var pre = root.getElementsByTagName('pre'); 
+		if (pre == null) {
+			return;
+		}
+
+		for (var i = 0; i < pre.length; i++) {
+			var count;
+			var content;
+
+			content = pre[i].innerHTML;
+			content = content.replace(/\n+$/g, '');
+			content = content.replace(/\t/g, '    ');
+			pre[i].innerHTML = content;
+
+			/* IE replaces '\n' by a space; this degrades to match nothing. */
+			var matches = pre[i].innerHTML.match(/\n/mg);
+			if (matches == null) {
+				count = 1;
+			} else {
+				count = matches.length + 1;
+			}
+
+			var numbers = [];
+			for (var n = 1; n <= count; n++) {
+				numbers.push(n);
+			}
+			numbers = numbers.join("\n");
+
+			var div = root.ownerDocument.createElementNS('http://www.w3.org/1999/xhtml', 'div');
+			div.appendChild(root.ownerDocument.createTextNode(numbers));
+			div.setAttribute('class', 'lines');
+			pre[i].appendChild(div);
+			pre[i].insertBefore(div, pre[i].firstChild);
+		}
+	}
+
+});
+
---
+++ vhost/www.libfsm.org/js/javascript-xpath-cmp.js
@@ -0,0 +1,1 @@
+../../../js/javascript-xpath-cmp.js
