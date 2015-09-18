0cd8de440921bf21c3bd2563d0014f75eb82a2bc
Kate F <kate@elide.org>
2015-09-18 18:07:51 +0000

    Revert [578]; there may be only one `<thead>` in a table, which makes this redundant.

--- expander/expander.js
+++ expander/expander.js
@@ -71,15 +71,9 @@ var Expander = new (function () {
 	}
 
 	this.toggle = function (a, accordion, expand) {
-		var dl, dt;
+		var dl, dt = a.parentNode;
 		var endclass;
 		var r;
-
-		if (a.nodeName.toLowerCase() == 'thead') {
-			dt = a;
-		} else {
-			dt = a.parentNode;
-		}
 
 		for (dl = dt; dl != null; dl = dl.parentNode) {
 			if (hasclass(dl, "expandable")) {
@@ -130,27 +124,15 @@ var Expander = new (function () {
 
 			var dt = dl[i].getElementsByTagName(dtname);
 			for (var j = 0; j < dt.length; j++) {
-				var a;
-
-				if (dt[j].nodeName.toLowerCase() == 'th'
-				 && dt[j].parentNode.nodeName.toLowerCase() == 'tr'
-				 && dt[j].parentNode.parentNode.nodeName.toLowerCase() == 'thead')
-				{
-					/* special case for collapsing thead/tbody within tables;
-					 * use thead for onclick */
-					a = dt[j].parentNode.parentNode;
+				var a = dt[j].getElementsByTagName("a");
+				if (a.length > 0) {
+					a = a[0];
 				} else {
+					a = document.createElementNS('http://www.w3.org/1999/xhtml', 'a');
+					a.innerHTML  = dt[j].innerHTML;
 
-					a = dt[j].getElementsByTagName("a");
-					if (a.length > 0) {
-						a = a[0];
-					} else {
-						a = document.createElementNS('http://www.w3.org/1999/xhtml', 'a');
-						a.innerHTML  = dt[j].innerHTML;
-
-						dt[j].innerHTML = '';
-						dt[j].appendChild(a);
-					}
+					dt[j].innerHTML = '';
+					dt[j].appendChild(a);
 				}
 
 				a.onclick = function () {
