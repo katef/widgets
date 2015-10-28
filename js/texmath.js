f69675693ea4f0835b308d6f7ae9f6c02f5815d4
Kate F <kate@elide.org>
2015-10-28 21:13:08 +0000

    A thing for picking out TeX elements to feed to KaTeX.

---
+++ js/texmath.js
@@ -0,0 +1,15 @@
+/* $Id$ */
+
+var TexMath = new (function () {
+
+	this.init = function(root) {
+		var a;
+
+		a = root.getElementsByClassName('texmath');
+		for (i = 0; i < a.length; i++) {
+		   katex.render(a[i].textContent, a[i]);
+		}
+	}
+
+});
+
