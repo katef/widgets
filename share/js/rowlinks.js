53dfccb21e9377ccef3b9fdbe102033b357679d1
Kate F <kate@elide.org>
2016-01-25 00:21:45 +0000

    Revamped.

--- share/js/rowlinks.js
+++ share/js/rowlinks.js
@@ -1,27 +1,59 @@
-/* adapted from http://radio.javaranch.com/pascarello/2005/05/19/1116509823591.html */
-function ConvertRowsToLinks(xTableId) {
-	var rows;
+/*
+ * <body onload="ConvertRowsToLinks('dl')">
+ * Adapted from http://radio.javaranch.com/pascarello/2005/05/19/1116509823591.html
+ */
 
-	rows = document.getElementById(xTableId).getElementsByTagName("tr");
-	for (i = 0; i < rows.length; i++) {
-		var link;
-		var klass;
+var Rowlinks = new (function () {
 
-		link = rows[i].getElementsByTagName("a")
-		if (link.length != 1) {
-			continue;
+	/* see http://elide.org/snippets/css.js */
+	function addclass(node, klass) {
+		var a, c;
+
+		a = [ ];
+
+		c = node.getAttribute('class');
+		if (c != null) {
+			a = c.split(/\s/);
 		}
 
-		rows[i].onclick = new Function("document.location.href='" + link[0].href + "'");
-
-		klass = rows[i].getAttribute('class');
-		if (klass == null) {
-			klass = '';
+		for (var i = 0; i < a.length; i++) {
+			if (a[i] == klass || a[i] == '') {
+				a.splice(i, 1);
+				i--;
+			}
 		}
 
-		rows[i].setAttribute('class', klass + ' ' + xTableId);
+		a.push(klass);
+
+		node.setAttribute('class', a.join(' '));
 	}
-}
 
-/* <body onload="ConvertRowsToLinks('dl')"> */
+	this.convert = function (t) {
+		var rows;
+
+		rows = t.getElementsByTagName("tr");
+		for (i = 0; i < rows.length; i++) {
+			var link;
+
+			link = rows[i].getElementsByTagName("a")
+			if (link.length == 0) {
+				continue;
+			}
+
+			rows[i].onclick = new Function("document.location.href='" + link[0].href + "'");
+
+			addclass(rows[i], "rowlink");
+		}
+	}
+
+	this.init = function (root) {
+		var a;
+
+		a = root.getElementsByTagName('table');
+		for (var i = 0; i < a.length; i++) {
+			convert(a[i]);
+		}
+	}
+
+});
 
