fb6f20bdefe3a316f9d5d354d85e81e0ff26a571
Kate F <kate@elide.org>
2016-02-29 21:18:58 +0000

    Mutually exclusive expandion for HTML5 `<details>` expanders.

---
+++ share/js/accordion.js
@@ -0,0 +1,35 @@
+/* $Id$ */
+
+var Accordion = new (function () {
+
+	this.init = function (root) {
+		var div = root.getElementsByTagName('div');
+
+		for (var i = 0; i < div.length; i++) {
+			if (!div[i].classList.contains('accordion')) {
+				continue;
+			}
+
+			var details = div[i].getElementsByTagName("details");
+			for (var j = 0; j < details.length; j++) {
+				// possible to share guts with expander js?
+				EventThing.attach(details[j], 'toggle', function () {
+					if (!this.open) {
+						return;
+					}
+
+					var details = this.parentNode.getElementsByTagName("details");
+					for (var k = 0; k < details.length; k++) {
+						if (details[k] === this) {
+							continue;
+						}
+
+						details[k].open = false;
+					}
+				});
+			}
+		}
+	}
+
+});
+
